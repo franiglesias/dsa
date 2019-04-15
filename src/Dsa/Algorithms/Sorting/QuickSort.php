4e5cc6cdb8de1d7ad659f6671a536eff6a0a331f
franiglesias <fran.iglesias@holaluz.com>
2019-04-15 17:44:51 +0200

    Refactor

--- src/Dsa/Algorithms/Sorting/QuickSort.php
+++ src/Dsa/Algorithms/Sorting/QuickSort.php
@@ -4,22 +4,27 @@ namespace Dsa\Algorithms\Sorting;
 
 class QuickSort
 {
-    public function sort(array $source)
+    public function sort(array $source): array
     {
         $length = count($source);
         if ($length <= 1) {
             return $source;
         }
+
         $pivot = $this->median($source);
         $equal = $less = $greater = [];
         for ($i = 0; $i < $length; $i++) {
-            if ($source[$i] == $pivot) {
-                $equal[] = $source[$i];
-            } elseif ($source[$i] < $pivot) {
-                $less[] = $source[$i];
-            } else {
+            if ($source[$i] > $pivot) {
                 $greater[] = $source[$i];
+                continue;
             }
+
+            if ($source[$i] < $pivot) {
+                $less[] = $source[$i];
+                continue;
+            }
+
+            $equal[] = $source[$i];
         }
 
         return array_merge($this->sort($less), $equal, $this->sort($greater));
--- src/Dsa/Structures/BinarySearchTree.php
+++ src/Dsa/Structures/BinarySearchTree.php
@@ -9,95 +9,114 @@ class BinarySearchTree
      */
     private $root;
 
-
-    public function insert($value)
+    public function insert($value): void
     {
         $new = new BinarySearchNode($value);
-        if (!$this->root) {
+        if (! $this->root) {
             $this->root = $new;
-        } else {
-            $this->insertNew($this->root, $new);
+            return;
         }
+
+        $this->insertNew($this->root, $new);
     }
 
-    public function insertNew(BinarySearchNode $current, BinarySearchNode $new)
-    {
+    public function insertNew(
+        BinarySearchNode $current,
+        BinarySearchNode $new
+    ) {
         if ($new->getValue() < $current->getValue()) {
-            if (!$current->getLeft()) {
+            if (! $current->getLeft()) {
                 $current->setLeft($new);
-            } else {
-                $this->insertNew($current->getLeft(), $new);
+
+                return;
             }
-        } else {
-            if (!$current->getRight()) {
-                $current->setRight($new);
-            } else {
-                $this->insertNew($current->getRight(), $new);
-            }
+
+            $this->insertNew($current->getLeft(), $new);
+
+            return;
         }
+
+        if (! $current->getRight()) {
+            $current->setRight($new);
+
+            return;
+        }
+
+        $this->insertNew($current->getRight(), $new);
     }
 
     public function isInTree($value)
     {
-        if (!$this->root) {
+        if (! $this->root) {
             return false;
         }
+
         return $this->contains($this->root, $value);
     }
 
-    public function contains(BinarySearchNode $current = null, $value)
-    {
-        if (!$current) {
+    public function contains(
+        BinarySearchNode $current = null,
+        $value
+    ) {
+        if (! $current) {
             return false;
         }
-        return $this->findNode($current, $value) ? true:false;
 
+        return $this->findNode($current, $value) ? true : false;
     }
 
     public function getParentValueOf($value)
     {
-        if ($value == $this->root->getValue()){
+        if ($value == $this->root->getValue()) {
             return null;
         }
 
         return $this->findParent($this->root, $value)->getValue();
     }
 
-    private function findParent(BinarySearchNode $current, $value)
-    {
+    private function findParent(
+        BinarySearchNode $current,
+        $value
+    ) {
         if ($value < $current->getValue()) {
-            if (!$current->getLeft()) {
+            if (! $current->getLeft()) {
                 return null;
-            } elseif ($current->getLeft()->getValue() == $value) {
-                return $current;
-            } else {
-                return $this->findParent($current->getLeft(), $value);
             }
-        } else {
-            if (!$current->getRight()) {
-                return null;
-            } elseif ($current->getRight()->getValue() == $value) {
+
+            if ($current->getLeft()->getValue() === $value) {
                 return $current;
-            } else {
-                return $this->findParent($current->getRight(), $value);
             }
+
+            return $this->findParent($current->getLeft(), $value);
         }
+
+        if (! $current->getRight()) {
+            return null;
+        }
+
+        if ($current->getRight()->getValue() == $value) {
+            return $current;
+        }
+
+        return $this->findParent($current->getRight(), $value);
     }
 
-    private function findNode(BinarySearchNode $current = null, $value)
-    {
-        if (!$current) {
+    private function findNode(
+        BinarySearchNode $current = null,
+        $value
+    ) {
+        if (! $current) {
             return false;
         }
 
-        if ($current->getValue() == $value) {
+        if ($current->getValue() === $value) {
             return $current;
-        } elseif ($value < $current->getValue()) {
-            return $this->findNode($current->getLeft(), $value);
-        } else {
-            return $this->findNode($current->getRight(), $value);
         }
 
+        if ($value < $current->getValue()) {
+            return $this->findNode($current->getLeft(), $value);
+        }
 
+        return $this->findNode($current->getRight(), $value);
     }
 }
